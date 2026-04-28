e39775c786fbf6d8b917384bf350eff9b5703f04
Marialopes432 <rosimeiretemoteo546@gmail.com>
2026-04-27 21:35:33 -0300

    meu primeiro commit

---
+++ inicio/string.js
@@ -0,0 +1,16 @@
+let nome = "lucas";
+let frase = "olá" + nome + ",\nseja bem-vindo!";
+console.log(frase);
+
+
+// template string
+frase = òlá ${nome}, 
+seja bem-vindo!`;
+console.log(frase);
+
+
+
+
+
+
+
